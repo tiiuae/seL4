f3d89b456f912ebb19a972015b1312710aa020c4
Jouni Raunio <jouni.raunio@unikie.com>
2021-06-09 16:30:16 +0300

    Non SMP support for trace.c

--- src/kernel/trace.c
+++ src/kernel/trace.c
@@ -10,7 +10,11 @@ trace_entry_t tes[4][1024];
 
 void __cyg_profile_func_enter(void *thisfn, void *call_site)
 {
+#ifdef ENABLE_SMP_SUPPORT
 	int cpu = getCurrentCPUIndex();
+#else
+	int cpu = 0;
+#endif
 	tes[cpu][te_head[cpu]].func = (unsigned long) thisfn;
 	te_head[cpu]++;
 	te_head[cpu] &= 1023;
